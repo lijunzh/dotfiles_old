bbb7f487a7b563cffba0b1f00bba2813c5dfe01c
Lijun Zhu <gatechzhu@gmail.com>
2020-05-12 12:01:21 -0500

    Use fd instead of rg for file search.

--- .zsh/configs/post/fzf.zsh
+++ .zsh/configs/post/fzf.zsh
@@ -13,9 +13,11 @@ fi
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
 # Configuration
-export FZF_DEFAULT_COMMAND='rg --files --hidden --ignore-case'
+# export FZF_DEFAULT_COMMAND='rg --files --hidden --ignore-case'
+export FZF_DEFAULT_COMMAND="fd --hidden --exclude '.git' . $HOME"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd -t d . $HOME"
 export FZF_DEFAULT_OPTS='
 	--multi --layout=reverse --border --inline-info
 	--bind ctrl-f:page-down,ctrl-b:page-up
 	--preview "bat --color 'always' {}"'
-	# --preview "(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200 {}"'
