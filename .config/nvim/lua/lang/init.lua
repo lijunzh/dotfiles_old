56014f832d699398af97876e28ccb52ae9ffdd15
Lijun Zhu <gatechzhu@gmail.com>
2021-07-09 15:51:34 -0500

    Add more functionality to lsp.

--- .config/nvim/lua/lang/init.lua
+++ .config/nvim/lua/lang/init.lua
@@ -23,16 +23,96 @@ local on_attach = function(client, bufnr)
 	buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
 	buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 	buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-	buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-	buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-	buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-	buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-	buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-	buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+	buf_set_keymap('n', '<Leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+	buf_set_keymap('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+	buf_set_keymap('n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
 	buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-	buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+	buf_set_keymap('n', '<Leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
 	buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
 	buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-	buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-	buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+	-- Set some keybinds conditional on server capabilities
+	if client.resolved_capabilities.document_formating then
+		buf_set_keymap("n", "<Leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+	elseif client.resolved_capabilities.document_range_formating then
+		buf_set_keymap("n", "<Leader>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+	end
+
+	-- Set autocommands conditional on server_capabilities
+	if client.resolved_capabilities.document_highlight then
+		vim.api.nvim_exec([[
+		hi LspReferenceRead cterm=bold ctermbg=red guibg=#282f45
+		hi LspReferenceText cterm=bold ctermbg=red guibg=#282f45
+		hi LspReferenceWrite cterm=bold ctermbg=red guibg=#282f45
+		augroup lsp_document_highlight
+		autocmd! * <buffer>
+		autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+		autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+		augroup END
+		]], false)
+	end
+end
+
+local nvim_lsp = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+-- Code actions
+capabilities.textDocument.codeAction = {
+	dynamicRegistration = true,
+	codeActionLiteralSupport = {
+		codeActionKind = {
+			valueSet = (function()
+				local res = vim.tbl_values(vim.lsp.protocol.CodeActionKind)
+				table.sort(res)
+				return res
+			end)()
+		}
+	}
+}
+
+capabilities.textDocument.completion.completionItem.snippetSupport = true;
+
+-- LSPs
+local servers = {"pyright", "rust_analyzer", "gopls", "tsserver", "vimls"}
+for _, lsp in ipairs(servers) do
+	nvim_lsp[lsp].setup {capabilities = capabilities, on_attach = on_attach}
+end
+
+-- symbols-outline.nvim
+vim.g.symbols_outline = {
+	highlight_hovered_item = true,
+	show_guides = true,
+	auto_preview = false, -- experimental
+	position = 'right',
+	keymaps = {
+		close = "<Esc>",
+		goto_location = "<Cr>",
+		focus_location = "o",
+		hover_symbol = "<C-space>",
+		rename_symbol = "r",
+		code_actions = "a"
+	},
+	lsp_blacklist = {}
+}
+
+-- Send diagnostics to quickfix list
+do
+	local method = "textDocument/publishDiagnostics"
+	local default_handler = vim.lsp.handlers[method]
+	vim.lsp.handlers[method] = function(err, method, result, client_id, bufnr,
+										config)
+		default_handler(err, method, result, client_id, bufnr, config)
+		local diagnostics = vim.lsp.diagnostic.get_all()
+		local qflist = {}
+		for bufnr, diagnostic in pairs(diagnostics) do
+			for _, d in ipairs(diagnostic) do
+				d.bufnr = bufnr
+				d.lnum = d.range.start.line + 1
+				d.col = d.range.start.character + 1
+				d.text = d.message
+				table.insert(qflist, d)
+			end
+		end
+		vim.lsp.util.set_qflist(qflist)
+	end
 end
