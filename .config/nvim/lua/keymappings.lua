465d01bc7d8cdf64b4a782aa152df0c91cdd014a
Lijun Zhu <gatechzhu@gmail.com>
2021-07-23 09:25:56 -0500

    Update keymappings to better match vim commands.

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -1,6 +1,6 @@
 local utils = require('utils')
 
 utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
-utils.map('n', '<Leader>w', '<Cmd>update<CR>')
+utils.map('n', '<Leader>s', '<Cmd>update<CR>')
 utils.map('n', '<Leader>q', '<Cmd>q<CR>')
-utils.map('n', '<Leader>fw', '<Cmd>w<CR>')
+utils.map('n', '<Leader>w', '<Cmd>w<CR>')
